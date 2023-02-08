22a766aa4c61b36f1dd75085cce9f5774fc4c69c
joom <113173627+joom122796@users.noreply.github.com>
2023-02-08 20:32:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def happy(num):
     check = []
+    check.sort()
     list = [int(i) for i in str(num)]
     while num != 1:
         num = sum(num**2 for i in str(num))
         if num in check:
             print(f'{num} is not a happy number')
         check.append(num)
+        check.sort()
     print(f'{num} is a happy number')
 
 number = int(input('Enter number'))
